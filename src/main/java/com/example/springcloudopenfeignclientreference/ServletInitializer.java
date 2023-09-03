8abd6f181d408a0196a4470cece9a18a1d110b12
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-09-03 18:43:54 +0300

    init commit

---
+++ src/main/java/com/example/springcloudopenfeignclientreference/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.springcloudopenfeignclientreference;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringCloudOpenFeignClientReferenceApplication.class);
+	}
+
+}
---
+++ src/main/java/com/example/springcloudopenfeignclientreference/SpringCloudOpenFeignClientReferenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudopenfeignclientreference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOpenFeignClientReferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudOpenFeignClientReferenceApplication.class, args);
+	}
+
+}
