74ce64dbd6521678890ad1737812c2128e25b6d0
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-09-03 18:53:11 +0300

    enable feign clients

--- src/main/java/com/example/springcloudopenfeignclientreference/SpringCloudOpenFeignClientReferenceApplication.java
+++ src/main/java/com/example/springcloudopenfeignclientreference/SpringCloudOpenFeignClientReferenceApplication.java
@@ -2,8 +2,10 @@ package com.example.springcloudopenfeignclientreference;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SpringCloudOpenFeignClientReferenceApplication {
 
 	public static void main(String[] args) {
